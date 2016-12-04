7f6da21a50249af96583bc861dd806ed69357915
Laurent Gaches <laurent@binimo.com>
2016-12-04 17:31:20 +0100

    fix newline

--- Scripts/generate_graphiql.sh
+++ Scripts/generate_graphiql.sh
@@ -1,6 +1,6 @@
 
 
-OUTPUT=`tr -s '\n' ' ' < ../Configuration/GraphiQL.html`
+OUTPUT=`cat ../Configuration/GraphiQL.html |  while read line; do echo -n "$line\n"; done`
 echo $OUTPUT > TMP_FILE
 OUTPUT2=`sed 's/["'"'"']/\\\"/g' TMP_FILE` 
 echo "let graphiqlHTML=\"$OUTPUT2\"" > ../Sources/GraphQLMiddleware/GraphiQL+HTML.swift
