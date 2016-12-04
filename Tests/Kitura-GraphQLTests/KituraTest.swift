bbc0d9ea584898913c7fc089d05464d4711e58e8
Laurent Gaches <laurent@binimo.com>
2016-12-04 09:53:57 +0100

    add test

---
+++ Tests/Kitura-GraphQLTests/KituraTest.swift
@@ -0,0 +1,84 @@
+/*
+ * Copyright IBM Corporation 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import XCTest
+
+@testable import KituraNet
+
+import Foundation
+import Dispatch
+
+protocol KituraTest {
+    func expectation(_ index: Int) -> XCTestExpectation
+    func waitExpectation(timeout t: TimeInterval, handler: XCWaitCompletionHandler?)
+}
+
+extension KituraTest {
+
+   func doTearDown() {
+  //       sleep(10)
+    }
+
+    func performServerTest(_ router: ServerDelegate, asyncTasks: @escaping (XCTestExpectation) -> Void...) {
+        do {
+            let server = try HTTPServer.listen(on: 8090, delegate: router)
+            let requestQueue = DispatchQueue(label: "Request queue")
+
+            for (index, asyncTask) in asyncTasks.enumerated() {
+                let expectation = self.expectation(index)
+                requestQueue.async() {
+                    asyncTask(expectation)
+                }
+            }
+
+            waitExpectation(timeout: 10) { error in
+                // blocks test until request completes
+                server.stop()
+                XCTAssertNil(error);
+            }
+        } catch {
+            XCTFail("Error: \(error)")
+        }
+    }
+
+    func performRequest(_ method: String, path: String, callback: @escaping ClientRequest.Callback, headers: [String: String]? = nil, requestModifier: ((ClientRequest) -> Void)? = nil) {
+        var allHeaders = [String: String]()
+        if  let headers = headers  {
+            for  (headerName, headerValue) in headers  {
+                allHeaders[headerName] = headerValue
+            }
+        }
+        allHeaders["Content-Type"] = "text/plain"
+        let options: [ClientRequest.Options] =
+                [.method(method), .hostname("localhost"), .port(8090), .path(path), .headers(allHeaders)]
+        let req = HTTP.request(options, callback: callback)        
+        if let requestModifier = requestModifier {
+            requestModifier(req)
+        }
+        req.end()
+    }
+}
+
+extension XCTestCase: KituraTest {
+    func expectation(_ index: Int) -> XCTestExpectation {
+        let expectationDescription = "\(type(of: self))-\(index)"
+        return self.expectation(description: expectationDescription)
+    }
+
+    func waitExpectation(timeout t: TimeInterval, handler: XCWaitCompletionHandler?) {
+        self.waitForExpectations(timeout: t, handler: handler)
+    }
+}
--- Tests/Kitura-GraphQLTests/Kitura_GraphQLTests.swift
+++ Tests/Kitura-GraphQLTests/Kitura_GraphQLTests.swift
@@ -1,6 +1,17 @@
 import XCTest
 import Graphiti
+import Kitura
+import KituraNet
+
+
 @testable import GraphQLMiddleware
+
+
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin
+#endif
 
 let schema = try! Schema<Void> { schema in
     schema.query = try ObjectType(name: "RootQueryType") { query in
@@ -10,15 +21,57 @@ let schema = try! Schema<Void> { schema in
     }
 }
 
-let graphql = GraphQLMiddleware(schema: schema, showGraphiQL: true, rootValue: noRootValue)
+
 
 class Kitura_GraphQLTests: XCTestCase {
-    func testExample() {
+    let router = Kitura_GraphQLTests.setupRouter()
 
-
-        XCTAssertNotNil(graphql)
+    override func tearDown() {
+        doTearDown()
     }
 
+    func testExample() {
+        performServerTest(router) { expectation in
+
+            
+
+            self.performRequest("get", path: "/graphql", callback: { response in
+                XCTAssertNotNil(response, "Response object was nil")
+                //response?.headers[""]
+                do {
+                    let responseString = try response!.readString()
+
+                    XCTAssertNotNil(responseString)
+                } catch {
+                    XCTFail("Can't read response body")
+                }
+
+                expectation.fulfill()
+            })
+
+
+            self.performRequest("post", path: "/graphql", callback: { response in
+                
+            })
+        }
+
+    }
+
+    static func setupRouter() -> Router {
+        let router = Router()
+        router.all(middleware: GraphQLMiddleware(schema: schema, showGraphiQL: true, rootValue: noRootValue))
+
+        router.get("/graphql") {_, response, next in
+            do {
+                try response.send("Graphql").end()
+            } catch let error {
+                print(error)
+            }
+            next()
+        }
+
+        return router
+    }
 
     static var allTests : [(String, (Kitura_GraphQLTests) -> () throws -> Void)] {
         return [
