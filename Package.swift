488c04730144df9e24a89446540d46e9701a88bf
Laurent Gaches <laurent@binimo.com>
2017-01-30 11:24:56 +0100

    update Kitura version to 1.6

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-GraphQL",
     dependencies: [
-      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 3),
+      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
       .Package(url: "https://github.com/GraphQLSwift/Graphiti.git", majorVersion: 0, minor: 2)     
     ],
     exclude: ["Configuration", "Scripts"]
