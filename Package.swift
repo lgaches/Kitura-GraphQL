dcca805bd5dab8b3c8868e0419b8499dab2e95fa
Laurent Gaches <laurent@binimo.com>
2017-02-28 13:40:07 +0100

    change dep repo

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Kitura-GraphQL",
     dependencies: [
       .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
-      .Package(url: "https://github.com/GraphQLSwift/Graphiti.git", majorVersion: 0, minor: 2)     
+      .Package(url: "https://github.com/lgaches/Graphiti.git", majorVersion: 0, minor: 2)     
     ],
     exclude: ["Configuration", "Scripts"]
 )
