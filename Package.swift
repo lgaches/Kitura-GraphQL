950eb4b0f0264cf4db8c0dbcb0af6d89c26513ac
Laurent Gaches <laurent@binimo.com>
2017-04-30 08:54:30 +0200

    bump to kitura 1.7

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Kitura-GraphQL",
     dependencies: [
-      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
-      .Package(url: "https://github.com/lgaches/Graphiti.git", majorVersion: 0, minor: 2)     
+      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
+      .Package(url: "https://github.com/lgaches/Graphiti.git", majorVersion: 0, minor: 3)     
     ],
     exclude: ["Configuration", "Scripts"]
 )
